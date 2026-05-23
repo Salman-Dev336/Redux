b447200b20951f6487ee5bcafd49243ea397bb80
Salman Khan <salmantechhub336@gmail.com>
2026-05-23 21:21:11 +0500

    changes

---
+++ src/redux/features/counter_Slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+ export const counterSlice = createSlice({
+    name : 'counter',
+    initialState:{
+        value = 0
+
+    },
+    reducers:{
+        increment :()=>{
+
+        },
+        decrement :()=>{
+            
+        }
+    }
+ })
