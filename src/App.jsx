bfe9ca82516fc9ffde4870a5c6ca0657685bb6c1
Salman Khan <salmantechhub336@gmail.com>
2026-05-25 23:48:45 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,51 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increaseby5, increment } from './redux/features/counterSlice'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increaseby5,
+  increment,
+} from "./redux/features/counterSlice";
 
 const App = () => {
-
-  const dispatch = useDispatch()
-  const count = useSelector((state)=>
-    state.counter.value
-  )
-  const [num, setnum] = useState(5)
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+  const [num, setnum] = useState(5);
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={()=>{
-        dispatch(increment())
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        decrement
+      </button>
 
-      }}>Increment</button>
-      <button onClick={()=>{
-        dispatch(decrement())
+      <input
+      value={num}
+        type="number"
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setnum(e.target.value);
+        }}
+      />
 
-      }}>decrement</button>
-
-       <input type="number"  onChange={(e)=>{
-        console.log(e.target.value);
-        
-
-       }}  />
-
-
-      <button onClick={()=>{
-        dispatch(increaseby5(10))
-      }}>
-        increase by 10 
+      <button
+        onClick={() => {
+          dispatch(increaseby5(10));
+        }}
+      >
+        increase by 10
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
